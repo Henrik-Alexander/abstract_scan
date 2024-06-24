f3a8209685e2e45d847e93296c46c78faed1a816
Henrik-Alexander Schubert <77537729+Henrik-Alexander@users.noreply.github.com>
2024-06-24 09:03:09 +0200

    Clean the functions.

--- functions.R
+++ functions.R
@@ -1,6 +1,14 @@
+### Abstract screening ######################
+# Project: Formal demography of fertility   #
+# Purpose: Supporting functions	            #
+# Author: Henrik-Alexander Schubert         #
+# E-mail: schubert@demogr.mpg.de            #
+# Date: 14.06.2024                          #
+#############################################
 
 
-# Translate to outcome
+
+# Harmonize the outcome across different settings
 harmonize_outcomes <- function(data){
   columns <- str_detect(names(data), "screening")
   if(!any(columns)){
@@ -14,7 +22,7 @@ harmonize_outcomes <- function(data){
   }
 }
 
-# Translate the ooutcomes
+# Translate the outcomes to their meaning
 translate_answers <- function(answer) {
   if (answer == yes){
     return("yes")
@@ -24,40 +32,3 @@ translate_answers <- function(answer) {
     return("maybe") 
   }
 }
-
-
-# Create the result vector
-assign_answers <- function(df) {
-  result <- rep(NA_character_, length = length(df))
-  i <- 1
-  while (i < nrow(df)) {
-    cat("-------------------------------\n ")
-    cat("Title: \n", df$title[i], "\n")
-    cat("\n")
-    cat("Abstract: \n", df$abstract[i], "\n")
-    answer <- readline(paste0("Is this contribution fitting the definition of formal demography of fertility? \n"))
-    if (!(answer %in% c(yes, maybe, no, "exit"))) {
-      cat("You entered an incorrect answer! You have to give an answer again. \n")
-    } else if (answer == "repeat") {
-      i <- i - 1
-    }else if (answer == "exit"){
-      cat("Please run the remainder of the file in order to save the data. \n")
-      cat("Then, rerun the entire file. It will only load the non-answered abstracts and the last one.")
-      # Harmonize the result
-      df$result <- harmonize_outcomes(df)
-      break 
-      return(df)
-    }  else {
-      cat("Your answer for the last title was:", translate_answers(answer), "\n")
-      result[i] <- answer
-      i <- i + 1
-      if(i %% 20 == 0) print("You can leave by pressing Esc!")
-    }
-  }
-  
-  # Harmonize the result
-  df$result <- harmonize_outcomes(df)
-  
-  return(df)
-}
-
